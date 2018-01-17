005c6cbe298dac8ab6e0a080c03a16084675238a
hong <15731970130@163.com>
2018-01-17 20:24:42 +0800

    git测试

--- test.php
+++ test.php
@@ -4,6 +4,9 @@
  * @Author: hong
  * @Date:   2018-01-17 20:16:41
  * @Last Modified by:   hong
- * @Last Modified time: 2018-01-17 20:16:56
+ * @Last Modified time: 2018-01-17 20:24:01
  */
 echo "hello world";
+$str=array(1,3,5,5);
+$str=array_reverse($str);
+var_dump($str);
